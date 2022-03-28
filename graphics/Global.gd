20d83cf220c711b036fa67006a3881367f8c18a9
sallygamer07 <73270350+sallygamer07@users.noreply.github.com>
2022-03-28 15:51:51 +0900

    Add files via upload

---
+++ graphics/Global.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var player_wand
+var player_bow
+var player
+var player_pos
+var magic
+var boss
+var boss_skill
+var field_boss
+var field_boss_defeat
+var LPS
+var LPS_wand
+
+var node_creation_parent
+
+var from_level
+
+var loaded = -1
+var mainMenu
+
+var player_in_shop
+var active = false
+var active_shop = false
+
+var player_weapon
+
+var DayNightCycle
+
+func instance_node(node, location, parent):
+	var node_instance = node.instance()
+	parent.add_child(node_instance)
+	node_instance.global_position = location
+	return node_instance
---
+++ graphics/Global_Settings.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var music_volume : float
+var fx_volume : float
+
+var fullscreen = false
+
