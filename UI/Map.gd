14630af1dc4be5a5b45d7d6d980d5cefe0f5b0bf
sallygamer07 <73270350+sallygamer07@users.noreply.github.com>
2022-03-30 12:58:26 +0900

    Update Map.gd

--- UI/Map.gd
+++ UI/Map.gd
@@ -8,3 +8,5 @@ func _ready():
 	for button in get_tree().get_nodes_in_group("LevelBtn"):
 		if button.name == Global.player.data["level"]:
 			playerSprite.global_position = button.get_node("Sprite").global_position
+		else:
+			playerSprite.hide()
