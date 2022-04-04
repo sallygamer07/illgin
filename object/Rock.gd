2b9af6e56bb43820bf0a85adf493b599924b1af5
sallygamer07 <73270350+sallygamer07@users.noreply.github.com>
2022-04-04 17:31:42 +0900

    Add files via upload

---
+++ object/Rock.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+onready var area : Area2D = get_node("Object/Area2D")
+onready var anim : AnimationPlayer = get_node("AnimationPlayer")
+onready var rock = preload("res://object/Stone.tscn")
+
+var crash_num = 0
+var max_crash_num = 5
+
+func _ready():
+	var _rock_connect = area.connect("area_entered", self, "on_area_entered")
+	
+func _process(_delta):
+	if crash_num == max_crash_num:
+		var loot_randomizer : RandomNumberGenerator = RandomNumberGenerator.new()
+		loot_randomizer.randomize()
+		for _i in range(loot_randomizer.randi_range(1, 4)):
+			var rock_ins = rock.instance()
+			get_parent().add_child(rock_ins)
+			rock_ins.global_position = global_position
+		self.queue_free()
+	
+func on_area_entered(_area):
+	if _area.is_in_group("PlayerWeapon"):
+		crash_num += 1
+		anim.play("Shake")
---
+++ object/Tree.gd
@@ -0,0 +1,31 @@
+extends StaticBody2D
+class_name tree
+
+onready var area : Area2D = get_node("Object/Area2D")
+onready var wooden = preload("res://object/Wooden.tscn")
+onready var anim : AnimationPlayer = get_node("AnimationPlayer")
+
+var knockback_direction = Vector2.ZERO
+var knockback_force = 0
+
+var max_crash_num = 3
+var crash_num = 0
+
+func _ready():
+	var _tree_connect = area.connect("area_entered", self, "on_area_entered")
+	
+func _process(_delta):
+	if crash_num == max_crash_num:
+		var loot_randomizer : RandomNumberGenerator = RandomNumberGenerator.new()
+		loot_randomizer.randomize()
+		for _i in range(loot_randomizer.randi_range(1, 3)):
+			var wooden_ins = wooden.instance()
+			get_parent().add_child(wooden_ins)
+			wooden_ins.global_position = global_position
+		self.queue_free()
+	
+func on_area_entered(_area):
+	if _area.is_in_group("PlayerWeapon"):
+		crash_num += 1
+		anim.play("Shake")
+	
